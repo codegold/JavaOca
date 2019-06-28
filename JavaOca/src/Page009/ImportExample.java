be0277c6c8e8f08492416dec24776ea541549afb
«MaxRubshik» <rubshikmax@gmail.com>
2019-06-28 09:22:00 +0300

    upd

--- JavaOca/src/Page009/ImportExample.java
+++ JavaOca/src/Page009/ImportExample.java
@@ -1,4 +1,10 @@
 package Page009;
 
+import java.util.Random;
+
 public class ImportExample {
+    public static void main(String[] args) {
+        Random r = new Random(); // DOES NOT COMPILE
+        System.out.println(r.nextInt(10));
+    }
 }
