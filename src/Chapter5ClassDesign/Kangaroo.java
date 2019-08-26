8761398179764472a02120f8e509323aa53b6b94
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2019-08-26 08:37:43 +0300

    Add files via upload

---
+++ src/Chapter5ClassDesign/Kangaroo.java
@@ -0,0 +1,22 @@
+package src.Chapter5ClassDesign;
+
+public class Kangaroo extends Marsupial {
+    public static boolean isBiped() {
+        return true;
+    }
+
+    public void getKangarooDescription() {
+        System.out.println("Kangaroo hops on two legs: " + isBiped());
+    }
+
+    public static void main(String[] args) {
+        Kangaroo joey = new Kangaroo();
+        joey.getMarsupialDescription();
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        joey.getKangarooDescription();
+    }
+}
