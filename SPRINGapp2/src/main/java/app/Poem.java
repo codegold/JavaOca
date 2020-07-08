b7a214f7d4067064145c97cefcaefccfd60bc84a
«MaxRubshik» <rubshikmax@gmail.com>
2020-07-08 13:33:46 +0300

    PoeticJuggler.

---
+++ SPRINGapp2/src/main/java/app/Poem.java
@@ -0,0 +1,5 @@
+package app;
+
+public interface Poem {
+    void recite();
+}
---
+++ SPRINGapp2/src/main/java/app/PoeticJuggler.java
@@ -0,0 +1,15 @@
+package app;
+
+public class PoeticJuggler extends Juggler{
+    private Poem poem;
+
+    public PoeticJuggler(Poem poem) {
+        super();
+        this.poem = poem;
+    }
+
+    public PoeticJuggler(int beanBags, Poem poem) {
+        super(beanBags);
+        this.poem = poem;
+    }
+}
