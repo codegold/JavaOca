c6ea29cdb4aa7db62f3ba21c6cad59e742a74943
«MaxRubshik» <rubshikmax@gmail.com>
2020-08-24 17:10:28 +0300

    Page class

---
+++ Spring_IO/interactWIthRestWebSErvice/src/main/java/com/exmanple/com/interactWIthRestWebSErvice/InteractWIthRestWebSErviceApplication.java
@@ -0,0 +1,13 @@
+package com.exmanple.com.interactWIthRestWebSErvice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractWIthRestWebSErviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractWIthRestWebSErviceApplication.class, args);
+	}
+
+}
---
+++ Spring_IO/interactWIthRestWebSErvice/src/test/java/com/exmanple/com/interactWIthRestWebSErvice/InteractWIthRestWebSErviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exmanple.com.interactWIthRestWebSErvice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractWIthRestWebSErviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
